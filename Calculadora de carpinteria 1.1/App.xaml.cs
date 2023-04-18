72c3ae98a690cc7b61bf6f1fd696015f232fba0f
Kaiser <cristianfiochetti@gmail.com>
2023-04-18 15:53:46 -0300

    Main changues

--- Calculadora de carpinteria 1.1/App.xaml.cs
+++ Calculadora de carpinteria 1.1/App.xaml.cs
@@ -8,7 +8,7 @@ public partial class App : Application
 	public App(ISecureStorageService secureStorage)
 	{
 		InitializeComponent();
-        MainPage = new NavigationPage(new ValidationView(secureStorage));
-        //MainPage = new NavigationPage(new MainPage());
+        //MainPage = new NavigationPage(new ValidationView(secureStorage));
+        MainPage = new NavigationPage(new MainPage());
     }
 }
